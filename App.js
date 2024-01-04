ae3a51d6f1512b3d97d96efef41ac111235662d0
Intan2109100 <119677639+Intan2109100@users.noreply.github.com>
2024-01-04 08:33:23 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import { Splash } from './component/page/Splash';
+import tw from 'twrnc';
+import { useEffect } from 'react';
+
+
+export default function App() {
+    return (
+        <View>
+            <Splash/>
+        </View>
+    );
+}
